5d6000f796200e5b4c6525727fa598b5785196f5
padammdev <91828761+nicolas-lcn@users.noreply.github.com>
2023-10-18 08:56:50 +0200

    created new scene world
    add material on hand
    raycast from camera to kill ghost
    (maybe interesting to find a way to not raycast everytime)

---
+++ Lenortini's Mansion/Assets/Scripts/FlashLightRayCaster.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightRayCaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if(hit.collider.CompareTag("Ghost"))
+            {
+                Debug.Log("Kill ghost");
+            }
+            
+            
+            
+        }
+    }
+}
